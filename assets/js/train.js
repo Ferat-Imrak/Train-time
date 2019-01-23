7b118bbc6d9a25c33be76c39af93cd7fb8a56c9e
Ferat Imrak <feratimrak1@gmail.com>
2019-01-23 00:06:54 -0600

    created html file and got firebase in js

---
+++ assets/js/train.js
@@ -0,0 +1,13 @@
+  
+  
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAQtZXXIV0nB-xcWE59Ju7eONE8fnWE67s",
+    authDomain: "trainferat.firebaseapp.com",
+    databaseURL: "https://trainferat.firebaseio.com",
+    projectId: "trainferat",
+    storageBucket: "",
+    messagingSenderId: "246485293528"
+  };
+  firebase.initializeApp(config);
